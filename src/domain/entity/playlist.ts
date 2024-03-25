5a603b840ec11cba661bcf23a56c751a756cb203
André Oliveira Silva <badecoandre1999@gmail.com>
2024-03-25 15:09:54 -0300

    feat: add queue and loop

--- src/domain/entity/playlist.ts
+++ src/domain/entity/playlist.ts
@@ -1,30 +1,39 @@
 export class Playlist {
   private tracks: Track[] = [];
   private _loop: boolean = false;
+  private queue: Queue<Track> = new Queue<Track>();
   constructor(private id: string, private name: string, tracks?: Track[]) {
     this.id = id;
     this.name = name;
     this.tracks = tracks || [];
+    this.tracks.forEach((track) => this.queue.enqueue(track));
   }
 
   public addTrack(track: Track): void {
     this.tracks.push(track);
+    this.queue.enqueue(track);
   }
 
   public removeTrack(trackId: string): void {
     this.tracks = this.tracks.filter((track) => track.getId() !== trackId);
-  }
-
-  public getCurrentTrack(): Track {
-    return this.tracks[0];
+    this.queue = new Queue<Track>();
+    this.tracks.forEach((track) => this.queue.enqueue(track));
   }
 
   public loop(value: boolean): void {
     this._loop = value;
   }
 
-  public getNextTrack(): Track {
-    return this.tracks[1];
+  public nextTrack(): Track {
+    const lastSong = this.queue.dequeue();
+    if (this._loop) {
+      this.queue.enqueue(lastSong);
+    }
+    return this.queue.peek();
+  }
+
+  public getCurrentTrack(): Track {
+    return this.queue.peek();
   }
 
   public getId(): string {
